692d4981a2a3c0e995d180b7cb3969cb2faa9f5d
Alan Gutierrez <alan@blogometer.com>
2010-09-07 22:00:51 -0500

    Test formatting to match Vows documentation.

--- vows/packet-test.js
+++ vows/packet-test.js
@@ -5,15 +5,15 @@ vows.describe('Packet').addBatch({
     'Packet can read': {
         topic: require('packet').create(),
         'a byte from a buffer': function (topic) {
-            var invoked = false;
-            topic.reset();
-            topic.packet('n8', function (field, engine) {
-                assert.equal(engine.bytesRead, 1);
-                assert.equal(field, 1);
-                invoked = true;
-            });
-            topic.read([ 1 ]);
-            assert.isTrue(invoked);
+              var invoked = false;
+              topic.reset();
+              topic.packet('n8', function (field, engine) {
+                  assert.equal(engine.bytesRead, 1);
+                  assert.equal(field, 1);
+                  invoked = true;
+              });
+              topic.read([ 1 ]);
+              assert.isTrue(invoked);
         },
         'a 16 bit number from a buffer': function (topic) {
             var invoked = false;
@@ -51,45 +51,45 @@ vows.describe('Packet').addBatch({
         },
         'a signed byte from a buffer': function (topic) {
             function readSigned(bytes, value) {
-              var invoked = false;
-              topic.reset();
-              topic.packet('-n8', function (field, engine) {
-                  assert.equal(engine.bytesRead, 1);
-                  assert.equal(field, value);
-                  invoked = true;
-              });
-              topic.read(bytes);
-              assert.isTrue(invoked);
+                var invoked = false;
+                topic.reset();
+                topic.packet('-n8', function (field, engine) {
+                    assert.equal(engine.bytesRead, 1);
+                    assert.equal(field, value);
+                    invoked = true;
+                });
+                topic.read(bytes);
+                assert.isTrue(invoked);
             }
             readSigned([ 0x80 ], -128);
             readSigned([ 0xff ], -1);
         },
         'a signed short from a buffer': function (topic) {
             function readSigned(bytes, value) {
-              var invoked = false;
-              topic.reset();
-              topic.packet('-b16', function (field, engine) {
-                  assert.equal(engine.bytesRead, 2);
-                  assert.equal(field, value);
-                  invoked = true;
-              });
-              topic.read(bytes);
-              assert.isTrue(invoked);
+                var invoked = false;
+                topic.reset();
+                topic.packet('-b16', function (field, engine) {
+                    assert.equal(engine.bytesRead, 2);
+                    assert.equal(field, value);
+                    invoked = true;
+                });
+                topic.read(bytes);
+                assert.isTrue(invoked);
             }
             readSigned([ 0x80, 0x00 ], -32768);
             readSigned([ 0xff, 0xff ], -1);
         },
         'a little-endian 32 bit hex string from a buffer': function (topic) {
             function readHexString(bytes, value) {
-              var invoked = false;
-              topic.reset();
-              topic.packet('l32h', function (field, engine) {
-                  assert.equal(engine.bytesRead, 4);
-                  assert.equal(field, value);
-                  invoked = true;
-              });
-              topic.read(bytes);
-              assert.isTrue(invoked);
+                var invoked = false;
+                topic.reset();
+                topic.packet('l32h', function (field, engine) {
+                    assert.equal(engine.bytesRead, 4);
+                    assert.equal(field, value);
+                    invoked = true;
+                });
+                topic.read(bytes);
+                assert.isTrue(invoked);
             }
             readHexString([ 0x80, 0x00, 0x00, 0x00 ], '00000080');
             readHexString([ 0xff, 0xff, 0xff, 0xff ], 'ffffffff');
@@ -97,39 +97,39 @@ vows.describe('Packet').addBatch({
         },
         'a big-endian 32 bit hex string from a buffer': function (topic) {
             function readHexString(bytes, value) {
-              var invoked = false;
-              topic.reset();
-              topic.packet('b32h', function (field, engine) {
-                  assert.equal(engine.bytesRead, 4);
-                  assert.equal(field, value);
-                  invoked = true;
-              });
-              topic.read(bytes);
-              assert.isTrue(invoked);
+                var invoked = false;
+                topic.reset();
+                topic.packet('b32h', function (field, engine) {
+                    assert.equal(engine.bytesRead, 4);
+                    assert.equal(field, value);
+                    invoked = true;
+                });
+                topic.read(bytes);
+                assert.isTrue(invoked);
             }
             readHexString([ 0x80, 0x00, 0x00, 0x00 ], '80000000');
             readHexString([ 0xff, 0xff, 0xff, 0xff ], 'ffffffff');
             readHexString([ 0xA0, 0xB0, 0xC0, 0xD0 ], 'a0b0c0d0');
         },
         'a 64 bit float from a buffer': function (topic) {
-          function readSingleFloat(bytes, value) {
-            var invoked = false;
-            topic.reset();
-            topic.packet("b64f", function (field, engine) {
-              assert.equal(engine.bytesRead, 8);
-              assert.equal(field, value);
-              invoked = true;
-            });
-            topic.read(bytes);
-            assert.isTrue(invoked);
-          }
-          readSingleFloat([ 0xdb, 0x01, 0x32, 0xcf, 0xf6, 0xee, 0xc1, 0xc0 ], -9.1819281981e3);
-          readSingleFloat([ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x24, 0xc0 ], -10); 
+            function readSingleFloat(bytes, value) {
+                var invoked = false;
+                topic.reset();
+                topic.packet("b64f", function (field, engine) {
+                    assert.equal(engine.bytesRead, 8);
+                    assert.equal(field, value);
+                    invoked = true;
+                });
+                topic.read(bytes);
+                assert.isTrue(invoked);
+            }
+            readSingleFloat([ 0xdb, 0x01, 0x32, 0xcf, 0xf6, 0xee, 0xc1, 0xc0 ], -9.1819281981e3);
+            readSingleFloat([ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x24, 0xc0 ], -10); 
         }
-    }
-    , 'Packet can write':
-        { topic: require('packet').create()
-        , 'a byte': function (topic) {
+    },
+    'Packet can write': {
+        topic: require('packet').create(),
+        'a byte': function (topic) {
             var buffer = [];
             topic.reset();
             topic.send("n8", 0x01, function (engine) { 
@@ -137,8 +137,8 @@ vows.describe('Packet').addBatch({
             });
             topic.write(buffer, 0, 1);
             assert.equal(buffer[0], 0x01);
-        }
-        , 'a little-endian 16 bit integer': function (topic) {
+        },
+        'a little-endian 16 bit integer': function (topic) {
             var buffer = [];
             topic.reset();
             topic.send("l16", 0x1FF, function (engine) { 
@@ -146,8 +146,8 @@ vows.describe('Packet').addBatch({
             });
             topic.write(buffer, 0, 2);
             assert.deepEqual(buffer, [  0xFF, 0x01 ]);
-        }
-        , 'a big-endian 16 bit integer': function (topic) {
+        },
+        'a big-endian 16 bit integer': function (topic) {
             var buffer = [];
             topic.reset();
             topic.send("b16", 0x1FF, function (engine) { 
@@ -155,8 +155,8 @@ vows.describe('Packet').addBatch({
             });
             topic.write(buffer, 0, 2);
             assert.deepEqual(buffer, [  0x01, 0xFF ]);
-        }
-        , 'a little-endian 16 bit integer followed by a big-endian 16 bit integer': function (topic) {
+        },
+        'a little-endian 16 bit integer followed by a big-endian 16 bit integer': function (topic) {
             var buffer = [];
             topic.reset();
             topic.send("l16b16", 0x1FF, 0x1FF, function (engine) { 
@@ -168,4 +168,4 @@ vows.describe('Packet').addBatch({
     }
 }).export(module);
 
-/* vim: set ts=2 sw=2 et tw=0 nowrap: */
+/* vim: set ts=4 sw=4 et tw=0 nowrap: */
--- vows/pattern-test.js
+++ vows/pattern-test.js
@@ -8,172 +8,174 @@ vows.describe('Pattern').addBatch({
           assert.isFunction(topic.parse);
         }
     },
-    'Pattern can parse':
-    { topic: require('__internal/pattern')
-    , 'a single byte pattern': function (topic) {
-        var field = topic.parse('n8');
-        assert.deepEqual(field, [
-          { signed: false
-          , bits: 8
-          , endianness: 'b'
-          , bytes: 1
-          , type: 'n'
-          , arrayed: false
-          }
-        ]);
-      }
-    , 'a single signed byte pattern': function (topic) {
-        var field = topic.parse('-n8');
-        assert.deepEqual(field, [
-          { signed: true
-          , bits: 8
-          , endianness: 'b'
-          , bytes: 1
-          , type: 'n'
-          , arrayed: true
-          }
-        ]);
-      }
-    , 'a single 16 bit number pattern': function (topic) {
-        var field = topic.parse('n16');
-        assert.deepEqual(field, [
-          { signed: false
-          , bits: 16
-          , endianness: 'b'
-          , bytes: 2
-          , type: 'n'
-          , arrayed: false
-          }
-        ]);
-      }
-    , 'a single signed 16 bit number pattern': function (topic) {
-        var field = topic.parse('-n16');
-        assert.deepEqual(field, [
-          { signed: true
-          , bits: 16
-          , endianness: 'b'
-          , bytes: 2
-          , type: 'n'
-          , arrayed: true
-          }
-        ]);
-      }
-    , 'a single big-endian 16 bit number pattern': function (topic) {
-        var field = topic.parse('b16');
-        assert.deepEqual(field, [
-          { signed: false
-          , bits: 16
-          , endianness: 'b'
-          , bytes: 2
-          , type: 'n'
-          , arrayed: false
-          }
-        ]);
-      }
-    , 'a single little-endian 16 bit number pattern': function (topic) {
-        var field = topic.parse('l16');
-        assert.deepEqual(field, [
-          { signed: false
-          , bits: 16
-          , endianness: 'l'
-          , bytes: 2
-          , type: 'n'
-          , arrayed: false
-          }
-        ]);
-      }
-    , 'a single signed little-endian 16 bit number pattern': function (topic) {
-        var field = topic.parse('-l16');
-        assert.deepEqual(field, [
-          { signed: true
-          , bits: 16
-          , endianness: 'l'
-          , bytes: 2
-          , type: 'n'
-          , arrayed: true
-          }
-        ]);
-      }
-    , 'a single 16 bit skip pattern': function (topic) {
-        var field = topic.parse('s16');
-        assert.deepEqual(field, [
-          { signed: false
-          , bits: 16
-          , endianness: 's'
-          , bytes: 2
-          , type: 'n'
-          , arrayed: false
-          }
-        ]);
-      }
-    , 'a signed little-endian 16 bit number followed by a byte pattern': function (topic) {
-        var field = topic.parse('-l16n8');
-        assert.deepEqual(field, [
-          { signed: true
-          , bits: 16
-          , endianness: 'l'
-          , bytes: 2
-          , type: 'n'
-          , arrayed: true
-          }
-        , { signed: false
-          , bits: 8
-          , endianness: 'b'
-          , bytes: 1
-          , type: 'n'
-          , arrayed: false
-          }
-        ]);
-      }
-    , 'a 16 bit hex string.': function (topic) {
-        var field = topic.parse('l16h');
-        assert.deepEqual(field, [
-          { signed: false
-          , bits: 16
-          , endianness: 'l'
-          , bytes: 2
-          , type: 'h'
-          , arrayed: true
-          }
-        ]);
-      }
-    , 'a single 32 bit float pattern.': function (topic) {
-        var field = topic.parse('b32f');
-        assert.deepEqual(field, [
-          { signed: true
-          , bits: 32
-          , endianness: 'b'
-          , bytes: 4
-          , type: 'f'
-          , arrayed: true
-          }
-        ]);
-      }
-    , 'a single 64 bit float pattern.': function (topic) {
-        var field = topic.parse('b64f');
-        assert.deepEqual(field, [
-          { signed: true
-          , bits: 64
-          , endianness: 'b'
-          , bytes: 8
-          , type: 'f'
-          , arrayed: true
-          }
-        ]);
-      }
+    'Pattern can parse': {
+        topic: require('__internal/pattern'),
+        'a single byte pattern': function (topic) {
+            var field = topic.parse('n8');
+            assert.deepEqual(field, [
+                { signed: false
+                , bits: 8
+                , endianness: 'b'
+                , bytes: 1
+                , type: 'n'
+                , arrayed: false
+                }
+            ]);
+        },
+        'a single signed byte pattern': function (topic) {
+            var field = topic.parse('-n8');
+            assert.deepEqual(field, [
+                { signed: true
+                , bits: 8
+                , endianness: 'b'
+                , bytes: 1
+                , type: 'n'
+                , arrayed: true
+                }
+            ]);
+        },
+        'a single 16 bit number pattern': function (topic) {
+            var field = topic.parse('n16');
+            assert.deepEqual(field, [
+                { signed: false
+                , bits: 16
+                , endianness: 'b'
+                , bytes: 2
+                , type: 'n'
+                , arrayed: false
+                }
+            ]);
+        },
+        'a single signed 16 bit number pattern': function (topic) {
+            var field = topic.parse('-n16');
+            assert.deepEqual(field, [
+                { signed: true
+                , bits: 16
+                , endianness: 'b'
+                , bytes: 2
+                , type: 'n'
+                , arrayed: true
+                }
+            ]);
+        },
+        'a single big-endian 16 bit number pattern': function (topic) {
+            var field = topic.parse('b16');
+            assert.deepEqual(field, [
+                { signed: false
+                , bits: 16
+                , endianness: 'b'
+                , bytes: 2
+                , type: 'n'
+                , arrayed: false
+                }
+            ]);
+        },
+        'a single little-endian 16 bit number pattern': function (topic) {
+            var field = topic.parse('l16');
+            assert.deepEqual(field, [
+                { signed: false
+                , bits: 16
+                , endianness: 'l'
+                , bytes: 2
+                , type: 'n'
+                , arrayed: false
+                }
+            ]);
+        },
+        'a single signed little-endian 16 bit number pattern': function (topic) {
+            var field = topic.parse('-l16');
+            assert.deepEqual(field, [
+              { signed: true
+              , bits: 16
+              , endianness: 'l'
+              , bytes: 2
+              , type: 'n'
+              , arrayed: true
+              }
+            ]);
+        },
+        'a single 16 bit skip pattern': function (topic) {
+            var field = topic.parse('s16');
+            assert.deepEqual(field, [
+                { signed: false
+                , bits: 16
+                , endianness: 's'
+                , bytes: 2
+                , type: 'n'
+                , arrayed: false
+                }
+            ]);
+        },
+        'a signed little-endian 16 bit number followed by a byte pattern': function (topic) {
+            var field = topic.parse('-l16n8');
+            assert.deepEqual(field,
+            [
+                { signed: true
+                , bits: 16
+                , endianness: 'l'
+                , bytes: 2
+                , type: 'n'
+                , arrayed: true
+                }
+            , 
+                { signed: false
+                , bits: 8
+                , endianness: 'b'
+                , bytes: 1
+                , type: 'n'
+                , arrayed: false
+                }
+            ]);
+        },
+        'a 16 bit hex string.': function (topic) {
+            var field = topic.parse('l16h');
+            assert.deepEqual(field, [
+                { signed: false
+                , bits: 16
+                , endianness: 'l'
+                , bytes: 2
+                , type: 'h'
+                , arrayed: true
+                }
+            ]);
+        },
+        'a single 32 bit float pattern.': function (topic) {
+            var field = topic.parse('b32f');
+            assert.deepEqual(field, [
+                { signed: true
+                , bits: 32
+                , endianness: 'b'
+                , bytes: 4
+                , type: 'f'
+                , arrayed: true
+                }
+            ]);
+        },
+        'a single 64 bit float pattern.': function (topic) {
+            var field = topic.parse('b64f');
+            assert.deepEqual(field, [
+                { signed: true
+                , bits: 64
+                , endianness: 'b'
+                , bytes: 8
+                , type: 'f'
+                , arrayed: true
+                }
+            ]);
+        }
     },
-    'Pattern cannot parse':
-    { topic: require('__internal/pattern')
-    , 'a 7 bit pattern.': function (topic) {
-        assert.throws(function () {
-          topic.parse("b7");   
-        }, Error);
-      }
-    , 'a float pattern other than 32 or 64 bits.': function (topic) {
-        assert.throws(function () {
-          topic.parse('b16f');
-        }, Error);
-      }
+    'Pattern cannot parse': {
+        topic: require('__internal/pattern'),
+        'a 7 bit pattern.': function (topic) {
+            assert.throws(function () {
+                topic.parse("b7");   
+            }, Error);
+        },
+        'a float pattern other than 32 or 64 bits.': function (topic) {
+            assert.throws(function () {
+                topic.parse('b16f');
+            }, Error);
+        }
     }
 }).export(module);
 
