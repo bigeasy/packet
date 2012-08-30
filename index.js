f2123c199db5080c028c060d228d3cceada70a1a
Alan Gutierrez <alan@prettyrobots.com>
2012-08-30 17:21:12 -0400

    Benchmark displays percent increase or decrease.
    The benchmark program now displays a percentage increase or decrease in
    performance over the previous version.
    
    Our previous version is not necessarily the immediately previous
    version. We're going to continue to work on removing streaming and
    inheritence, comparing against the last version based on the streaming
    and inheritence code.

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
-var previous = require('./prev'), next = require('./next'), eq = require('assert').equal;
+var previous = require('./prev')
+  , next = require('./next')
+  , eq = require('assert').equal
+  , Benchmark = require('benchmark');
 
-function createBenchmark(packet) {
+var suite = new Benchmark.Suite;
+
+
+
+function createBenchmark(createSerializer, createParser) {
   return function () {
     var parser, serializer, buffer = new Buffer(1024), i, I;
-    for (i = 0; i < 7; i++) {
-      serializer = new (packet.Serializer);
-      serializer.packet('example', 'b16 => a, -b16 => b, b32 => c, -b32 => d, b8 => e, -b8 => f')
-      serializer.serialize('example', 0xffff, -1, 0x7ffffff, -1, 1, -1)
-      serializer.write(buffer);
-      parser = new (packet.Parser);
-      parser.packet('example', 'b16 => a, -b16 => b, b32 => c, -b32 => d, b8 => e, -b8 => f')
-      parser.extract('example', function (record) {
-        eq(0xffff, record.a);
-        eq(-1, record.b);
-        eq(0x7ffffff, record.c);
-        eq(-1, record.d);
-        eq(1, record.e);
-        eq(-1, record.f);
-      });
-      parser.parse(buffer);
-    }
-  }
-}
-
-function createResetBenchmark(packet) {
-  return function () {
-    var parser, serializer, buffer = new Buffer(1024), i, I;
-    serializer = new (packet.Serializer);
-    parser = new (packet.Parser);
-    serializer.packet('example', 'b16 => a, -b16 => b, b32 => c, -b32 => d, b8 => e, -b8 => f')
-    parser.packet('example', 'b16 => a, -b16 => b, b32 => c, -b32 => d, b8 => e, -b8 => f')
-    for (i = 0; i < 7; i++) {
+    serializer = createSerializer();
+    parser = createParser();
+    serializer.packet('example', 'b16 => a, x16{3}, -b16 => b, b32 => c, -b32 => d, b8 => e, -b8 => f')
+    parser.packet('example', 'b16 => a, x16{3}, -b16 => b, b32 => c, -b32 => d, b8 => e, -b8 => f')
+    for (i = 0; i < 27; i++) {
       serializer.reset();
       serializer.serialize('example', 0xffff, -1, 0x7ffffff, -1, 1, -1)
       serializer.write(buffer);
@@ -48,12 +32,18 @@ function createResetBenchmark(packet) {
   }
 }
 
-createBenchmark(previous)();
-createResetBenchmark(next)();
+function createSerializer () { return new (previous.Serializer) }
+function createParser () { return new (previous.Parser) }
 
-exports.compare =
-{ 'previous': createBenchmark(previous)
-, 'next': createResetBenchmark(next)
-};
+     createBenchmark(next.createSerializer, next.createParser)()
 
-require('bench').runMain();
+suite.add('previous', createBenchmark(createSerializer, createParser))
+     .add('next', createBenchmark(next.createSerializer, next.createParser))
+     .on('cycle', function(event) { console.log(String(event.target)); })
+     .on('complete', function() {
+        var fastest = this.filter('fastest'), slowest = this.filter('slowest');
+        var percent = (1 - (slowest[0].hz / fastest[0].hz)) * 100;
+        var adjective = fastest.pluck('name') == 'next' ? 'faster' : 'slower';
+        console.log('%d%% ' + adjective, percent.toFixed(2)); 
+     })
+     .run();
