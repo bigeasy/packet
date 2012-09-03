44d28654cf1e0f2e65c03ed36f00f1e9ffe8bc9a
Alan Gutierrez <alan@prettyrobots.com>
2012-09-02 21:49:19 -0400

    Added array and float example to benchmarks.
    Closes #48.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var previous = require('./prev')
   , next = require('./next')
   , eq = require('assert').equal
+  , deq = require('assert').deepEqual
   , Benchmark = require('benchmark');
 
 var suite = new Benchmark.Suite;
@@ -12,20 +13,25 @@ function createBenchmark(createSerializer, createParser) {
     var parser, serializer, buffer = new Buffer(1024), i, I;
     serializer = createSerializer();
     parser = createParser();
-    serializer.packet('example', 'b16 => a, x16{3}, -b16 => b, b32 => c, -b32 => d, b8 => e, -b8 => f')
-    parser.packet('example', 'b16 => a, x16{3}, -b16 => b, b32 => c, -b32 => d, b8 => e, -b8 => f')
+    var pattern = 'b16 => a, x16{3}, -b16 => b, b32 => c, -b32 => d, b8 => e, ' +
+      '-b8 => f, b32f => g, b32z => h, b32[4] => i';
+    serializer.packet('example', pattern);
+    parser.packet('example', pattern);
     for (i = 0; i < 27; i++) {
       serializer.reset();
-      serializer.serialize('example', 0xffff, -1, 0x7ffffff, -1, 1, -1)
+      serializer.serialize('example', 0xffff, -1, 0x7ffffff, -1, 1, -1, 0.1, [ 1, 2, 3, 4 ], [ 1, 2, 3, 4 ])
       serializer.write(buffer);
       parser.reset();
       parser.extract('example', function (record) {
-        eq(0xffff, record.a);
-        eq(-1, record.b);
-        eq(0x7ffffff, record.c);
-        eq(-1, record.d);
-        eq(1, record.e);
-        eq(-1, record.f);
+        eq(record.a, 0xffff);
+        eq(record.b, -1);
+        eq(record.c, 0x7ffffff);
+        eq(record.d, -1);
+        eq(record.e, 1);
+        eq(record.f, -1);
+        eq(record.g.toFixed(1), 0.1);
+        deq(record.h, [ 1, 2, 3, 4 ]);
+        deq(record.i, [ 1, 2, 3, 4 ]);
       });
       parser.parse(buffer);
     }
