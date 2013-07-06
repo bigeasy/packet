2e4cf9b73a87e49314a6902730027a94d8d743a9
Alan Gutierrez <alan@prettyrobots.com>
2013-07-06 12:15:02 -0400

    Fix buffer only tests in `parse/single.t.js`.
    The buffer only tests for big-endian singles did not actually invoke
    `parseEqual` with the `buffersOnly` option set.
    
    See #112.

--- t/parser/single.t.js
+++ t/parser/single.t.js
@@ -13,9 +13,9 @@ require('./proof')(60, function (parseEqual) {
   parseEqual({ require: true, subsequent: true }, "b32f", [ 0x00, 0x00, 0x20, 0xc1 ].reverse(), 4, -10, "negative, subsequent");
   parseEqual({ require: true }, "b32f", [ 0x00, 0x00, 0x20, 0xc1 ].reverse(), [ 1, 3 ], -10, "negative, fallback");
   parseEqual({ require: true }, "b32f", [ 0xcd, 0xcc, 0x2c, 0x41 ].reverse(), [ 1, 3 ], 10.800000190734863, "big positive, fallback");
-  parseEqual({ require: true }, "b8,b32f", [ 0xcd, 0xcc, 0x2c, 0x41, 0x01 ].reverse(), 5, 1, 10.800000190734863, "big positive, buffers only");
-  parseEqual({ require: true, subsequent: true }, "b8,b32f", [ 0xcd, 0xcc, 0x2c, 0x41, 0x01 ].reverse(), 5, 1, 10.800000190734863, "big positive, buffers only, subsequent");
-  parseEqual({ require: true }, "b8,b32f", [ 0xcd, 0xcc, 0x2c, 0x41, 0x01 ].reverse(), [ 0, 5 ], 1, 10.800000190734863, "big positive, buffers only, fallback");
+  parseEqual({ require: true, buffersOnly: true }, "b8,b32f", new Buffer([ 0xcd, 0xcc, 0x2c, 0x41, 0x01 ].reverse()), 5, 1, 10.800000190734863, "big positive, buffers only");
+  parseEqual({ require: true, buffersOnly: true, subsequent: true }, "b8,b32f", new Buffer([ 0xcd, 0xcc, 0x2c, 0x41, 0x01 ].reverse()), 5, 1, 10.800000190734863, "big positive, buffers only, subsequent");
+  parseEqual({ require: true, buffersOnly: true }, "b8,b32f", new Buffer([ 0xcd, 0xcc, 0x2c, 0x41, 0x01 ].reverse()), [ 0, 5 ], 1, 10.800000190734863, "big positive, buffers only, fallback");
   // Two in a row.
   parseEqual({ require: true }, "l32f,l32f", [ 0x00, 0x00, 0x20, 0xc1, 0xcd, 0xcc, 0x2c, 0x41 ], 8, -10, 10.800000190734863, "two in a row");
   parseEqual({ require: true, subsequent: true }, "l32f,l32f", [ 0x00, 0x00, 0x20, 0xc1, 0xcd, 0xcc, 0x2c, 0x41 ], 8, -10, 10.800000190734863, "two in a row, subsequent");
