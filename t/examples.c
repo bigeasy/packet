1930d1b43304e0e12e0a35be6c333e41fe13f512
Alan Gutierrez <alan@prettyrobots.com>
2013-07-06 10:53:47 -0400

    Update `examples.c` with network order.
    Added some `htonl` examples to `examples.c`. Fixed the output path to
    the `t` test directory.

--- t/examples.c
+++ t/examples.c
@@ -17,14 +17,21 @@ write(const char * filename, void * data, int length)
 int
 main()
 {
+    int i;
     char n8[] = { 127,  1, 0, -1, -2, -128 }; 
     unsigned char un8[] = { 255, 1, 0 };
-    double n64d[] = { -9.1819281981e3, -10, -0.11, 0, 0.11, 10 };
+    double n64f[] = { -9.1819281981e3, -10, -0.11, 0, 0.11, 10 };
     unsigned short int u16[] = { 65535, 65534, 1, 0 };
-    short int n16[] = { 32767, 128, 1, 0, -1, -129, -32768 };
-    write("vows/examples/n8", &un8, sizeof(un8));
-    write("vows/examples/-n8", &n8, sizeof(n8));
-    write("vows/examples/u16", &u16, sizeof(u16));
-    write("vows/examples/n16", &n16, sizeof(n16));
-    write("vows/examples/n64d", &n64d, sizeof(n64d));
+    short int s16[] = { 32767, 128, 1, 0, -1, -129, -32768 };
+    write("t/examples/n8", &un8, sizeof(un8));
+    write("t/examples/-n8", &n8, sizeof(n8));
+    write("t/examples/u16", &u16, sizeof(u16));
+    for (i = 0; i < sizeof(u16) / 2; i++) {
+      printf("%d %d\n", i, u16[i]);
+      u16[i] = htons(u16[i]);
+      printf("%d %d\n", i, u16[i]);
+    }
+    write("t/examples/n16", &u16, sizeof(u16));
+    write("t/examples/s16", &s16, sizeof(s16));
+    write("t/examples/n64f", &n64f, sizeof(n64f));
 }
