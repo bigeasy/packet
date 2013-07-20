f7d4cc24e810eacd2ef1b7b3f9aeed5f7986d050
Alan Gutierrez <alan@prettyrobots.com>
2013-07-20 14:44:55 -0400

    Choose fonts, set some margins.

--- edify.js
+++ edify.js
@@ -3,7 +3,7 @@ var edify = require('edify')
 
 module.exports = cadence(function (step, $, cache) {
     step(function () {
-        edify.marked($, '.description', step())
+        edify.marked($, '.description, .markdown', step())
     }, function () {
         edify.pygments($, '.lang-javascript', 'javascript', cache, step())
     })
