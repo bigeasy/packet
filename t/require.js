cc9ca7a71db11807b10b63168ef0c40ec060e944
Alan Gutierrez <alan@prettyrobots.com>
2013-09-26 15:31:21 +0000

    Add newline to end of generated source.
    Closes #245.

--- t/require.js
+++ t/require.js
@@ -2,7 +2,7 @@ module.exports = function (type, pattern, parameters, source) {
     var path = require('path'), builder = []
     builder.push('module.exports = function (' + parameters.join(', ') + ') {')
     builder.push.apply(builder, source.map(function (line) { return '  ' + line }))
-    builder.push('}')
+    builder.push('}', '')
 
     builder = builder.map(function (line) { return line.replace(/^\s+$/, '') })
 
