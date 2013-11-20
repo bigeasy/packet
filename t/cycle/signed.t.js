b3b222a8e98285cd570ee66f3d905b9233ce2a85
Alan Gutierrez <alan@prettyrobots.com>
2013-11-20 12:15:52 +0000

    Cycle test of max signed values.
    See #256.

--- t/cycle/signed.t.js
+++ t/cycle/signed.t.js
@@ -13,4 +13,18 @@ require('./proof')(0, function (cycle) {
         buffer:         [ 0xff ],
         types:          'bff'
     })
+    cycle({
+        message:        'signed',
+        pattern:        'foo: -b8',
+        object:         { foo: 127 },
+        buffer:         [ 0x7f ],
+        types:          'bff'
+    })
+    cycle({
+        message:        'signed',
+        pattern:        'foo: -b8',
+        object:         { foo: -128 },
+        buffer:         [ 0x80 ],
+        types:          'bff'
+    })
 })
