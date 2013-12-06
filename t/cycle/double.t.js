751def8b02849a8f1709f751916a6f3758b69379
Alan Gutierrez <alan@prettyrobots.com>
2013-12-05 22:06:54 -0800

    Cycle test of big-endian double float.
    See #252.

--- t/cycle/double.t.js
+++ t/cycle/double.t.js
@@ -1,9 +1,10 @@
 require('./proof')(0, function (cycle) {
     cycle({
         message:        'very negative',
-        pattern:        'foo: l64f',
+        pattern:        'foo: b64f',
         object:         { foo: -9.1819281981e3 },
         buffer:         [ 0xdb, 0x01, 0x32, 0xcf, 0xf6, 0xee, 0xc1, 0xc0 ],
+        buffer:         [ 0xc0, 0xc1, 0xee, 0xf6, 0xcf, 0x32, 0x01, 0xdb ],
         types:          'bff'
     })
 })
---
+++ t/generated/parse.bff.b64f_foo.js
@@ -0,0 +1,62 @@
+module.exports = function (object, callback) {
+    var inc
+
+    inc = function (buffer, start, end, index) {
+        var bite
+        var next
+        var _foo
+
+        this.parse = function (buffer, start, end) {
+            switch (index) {
+            case 0:
+                _foo = new ArrayBuffer(8)
+                bite = 7
+                index = 1
+            case 1:
+                while (bite != -1) {
+                    if (start == end) return start
+                    _foo[bite--] = buffer[start++]
+                }
+                object["foo"] = new DataView(_foo).getFloat64(0, true)
+            }
+
+            if (next = callback(object)) {
+                this.parse = next
+                return this.parse(buffer, start, end)
+            }
+
+            return start
+        }
+
+        return this.parse(buffer, start, end)
+    }
+
+    return function (buffer, start, end) {
+        var next
+        var _foo
+
+        if (end - start < 8) {
+            return inc.call(this, buffer, start, end, 0)
+        }
+
+        _foo = new ArrayBuffer(8)
+        _foo[7] = buffer[start]
+        _foo[6] = buffer[start + 1]
+        _foo[5] = buffer[start + 2]
+        _foo[4] = buffer[start + 3]
+        _foo[3] = buffer[start + 4]
+        _foo[2] = buffer[start + 5]
+        _foo[1] = buffer[start + 6]
+        _foo[0] = buffer[start + 7]
+        object["foo"] = new DataView(_foo).getFloat64(0, true)
+
+        start += 8
+
+        if (next = callback(object)) {
+            this.parse = next
+            return this.parse(buffer, start, end)
+        }
+
+        return start
+    }
+}
