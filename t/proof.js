77339e3acf8917be3941dc31b4e6ac0642349b08
Alan Gutierrez <alan@prettyrobots.com>
2015-11-28 00:52:28 -0600

    Remove `t/proof.js`.

--- t/proof.js
+++
@@ -1,6 +0,0 @@
-require('proof')(module, function (body, assert) {
-    var options = {}
-    options.precompiler = require('./require')
-    options.directory = 't/generated'
-    body(require('..').createPacketizer(options), assert)
-})
