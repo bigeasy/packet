246ec197b83c141fa8f441d8625887803444286f
Alan Gutierrez <alan@prettyrobots.com>
2017-05-06 13:00:48 -0500

    Rename sketch of parser.

--- t/sketch/minimal.t.js
+++ t/sketch/minimal.t.js
@@ -2,7 +2,7 @@ require('proof')(1, prove)
 
 function prove (assert) {
     var minimal = require('../language/source/minimal.packet.js')
-    var Parser = require('../../parse.sketch')
+    var Parser = require('../../parser.sketch')
     var Serializer = require('../../serializer.sketch')
 
     var toJSON = require('../to-json')
