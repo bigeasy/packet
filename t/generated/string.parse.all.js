42e3e25c2c2d09d98b31930e804ef701a5b00a4f
Alan Gutierrez <alan@prettyrobots.com>
2017-05-09 16:10:45 -0500

    Rename generated string parser file.

--- t/generated/string.parse.all.js
+++ t/generated/string.parse.all.js
@@ -4,15 +4,18 @@ module.exports = function (parsers) {
 
     parsers.all.object.prototype.parse = function (buffer, start) {
 
+        var index
         var object
+        var terminator
 
         object = {
-            integer: null
+            string: null
         }
 
-        object.integer =
-            buffer[start++] * 0x100 +
-            buffer[start++]
+        var terminator = [0]
+        var index = buffer.indexOf(new Buffer(terminator), start)
+        object.string = buffer.toString("utf8", start, index)
+        start = index + terminator.length
 
         return { start: start, object: object, parser: null }
     }
