0e38d62571e8768bfc63563500b77b5e5223eb0e
Alan Gutierrez <alan@prettyrobots.com>
2019-11-10 23:34:29 -0600

    Tidy `byte.t.js`.
    See #233.

--- test/cycle/byte.t.js
+++ test/cycle/byte.t.js
@@ -11,42 +11,4 @@ function prove (okay) {
         object: { word: 0xab },
         buffer: [ 0xab ]
     })
-
-    /*
-    cycle({
-        name: 'little-endian-word',
-        define: {
-            object: { word: ~16 }
-        },
-        object: { word: 0xabcd },
-        buffer: [ 0xcd, 0xab ]
-    })
-
-    cycle({
-        name: 'big-endian-word',
-        define: {
-            object: { word: 16 }
-        },
-        object: { word: 0xabcd },
-        buffer: [ 0xab, 0xcd ]
-    })
-
-    cycle({
-        name: 'big-endian-integer',
-        define: {
-            object: { integer: 32 }
-        },
-        object: { integer: 0x89abcdef },
-        buffer: [ 0x89, 0xab, 0xcd, 0xef ]
-    })
-
-    cycle({
-        name: 'little-endian-integer',
-        define: {
-            object: { integer: ~32 }
-        },
-        object: { integer: 0x89abcdef },
-        buffer: [ 0xef, 0xcd, 0xab, 0x89 ]
-    })
-        */
 }
