a99efc7e9612450013a32ad1d6aa45d548d8a47f
Alan Gutierrez <alan@prettyrobots.com>
2020-02-20 23:30:25 -0600

    Remove example code.
    See #406.

--- example/embedded.compile.bin.js
+++
@@ -1,8 +0,0 @@
-var path = require('path')
-var composer = require('../parse.all')
-var fs = require('fs')
-
-var source = fs.readFileSync(path.resolve(__dirname, 'embedded.packet.js'), 'utf8')
-var parsed = require('../parser').walk(source, '../packet')
-
-fs.writeFileSync('embedded.json', JSON.stringify(parsed, null, 2), 'utf8')
