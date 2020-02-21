b8b4feeee45f6c66d6aef11fbe352220a4254828
Alan Gutierrez <alan@prettyrobots.com>
2020-02-20 23:44:56 -0600

    Simplified langauge nested length-encoding.
    See #81.

---
+++ test/language/length-encoded-variable.t.js
@@ -0,0 +1,48 @@
+require('proof')(1, okay => {
+    const simplified = require('../../simplified')
+    okay(simplified({ packet: { value: [ 16, [{ first: [ 16, [ 16 ] ] }] ] } }), [{
+        name: 'packet',
+        fixed: false,
+        bits: 16,
+        type: 'structure',
+        lengthEncoded: true,
+        fields: [{
+            name: 'value',
+            type: 'lengthEncoding',
+            fixed: true,
+            bits: 16,
+            endianness: 'big',
+            compliment: false
+        }, {
+            name: 'value',
+            type: 'lengthEncoded',
+            fixed: false,
+            bits: 0,
+            element: {
+                type: 'structure',
+                bits: 16,
+                fixed: false,
+                fields: [{
+                    name: 'first',
+                    type: 'lengthEncoding',
+                    fixed: true,
+                    bits: 16,
+                    endianness: 'big',
+                    compliment: false
+                }, {
+                    name: 'first',
+                    type: 'lengthEncoded',
+                    fixed: false,
+                    bits: 0,
+                    element: {
+                        type: 'integer',
+                        fixed: true,
+                        bits: 16,
+                        endianness: 'big',
+                        compliment: false
+                    }
+                }]
+            }
+        }]
+    }], 'length-encoded-variable')
+})
