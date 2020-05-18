f3a3ea275ae795a63caac5c619c4bf66830223d6
Alan Gutierrez <alan@prettyrobots.com>
2020-05-18 15:49:25 -0500

    Sketch.

--- test/cycle/fixed-words.t.js
+++ test/cycle/fixed-words.t.js
@@ -4,9 +4,9 @@ function prove (okay) {
     require('./cycle')(okay, {
         name: 'array',
         define: {
-            object: { array: [ 0xaa, 0xaa, [ 4 ], [ 16 ] ]  }
+            object: { array: [ 0xd, 0xa, [ 16 ], [ 8 ] ]  }
         },
-        objects: [{ array: [ 0x0012, 0x4567, 0x890a ] }],
-        stopAt: 'sizeof'
+        objects: [{ array: Buffer.from('hello, world').toJSON().data }],
+        stopAt: 'serialize.all'
     })
 }
