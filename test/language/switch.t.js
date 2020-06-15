40a8ac9943f37e8c8ee8854391d9507a25aed95d
Alan Gutierrez <alan@prettyrobots.com>
2020-06-15 02:07:37 -0500

    Add variant packed switch to language.
    See #457.

--- test/language/switch.t.js
+++ test/language/switch.t.js
@@ -1,4 +1,4 @@
-require('proof')(3, okay => {
+require('proof')(4, okay => {
     const simplified = require('../../simplified')
     // TODO Come back and complete when you've implemented nested structures.
     okay(simplified({
@@ -223,4 +223,102 @@ require('proof')(3, okay => {
         ],
         name: 'object'
     }], 'variant switch')
+    okay(simplified({
+        object: {
+            header: [{
+                type: 2,
+                value: [ $ => $.type, [[
+                    0,  6,
+                ], [
+                    1, [ 'a', 2 ]
+                ]], { two: 2, four: 4 } ]
+            }, 8 ]
+        }
+    }), [{
+        dotted: '',
+        fixed: true,
+        bits: 8,
+        type: 'structure',
+        fields: [
+          {
+            type: 'integer',
+            dotted: '.header',
+            fixed: true,
+            bits: 8,
+            endianness: 'big',
+            compliment: false,
+            name: 'header',
+            fields: [{
+                type: 'integer',
+                dotted: '.type',
+                fixed: true,
+                bits: 2,
+                endianness: 'big',
+                compliment: false,
+                name: 'type'
+            }, {
+                type: 'switch',
+                stringify: false,
+                source: '$ => $.type',
+                bits: 6,
+                fixed: true,
+                cases: [{
+                    value: 0,
+                    fields: [{
+                        type: 'integer',
+                        dotted: '',
+                        fixed: true,
+                        bits: 6,
+                        endianness: 'big',
+                        compliment: false
+                    }]
+                }, {
+                    value: 1,
+                    fields: [{
+                        type: 'literal',
+                        dotted: '',
+                        ethereal: true,
+                        fixed: true,
+                        bits: 6,
+                        before: { repeat: 1, value: 'a', bits: 4 },
+                        fields: [{
+                            type: 'integer',
+                            dotted: '',
+                            fixed: true,
+                            bits: 2,
+                            endianness: 'big',
+                            compliment: false
+                        }],
+                        after: { repeat: 0, value: '' }
+                    }]
+                }],
+                otherwise: [{
+                    dotted: '',
+                    fixed: true,
+                    bits: 6,
+                    type: 'structure',
+                    fields: [{
+                        type: 'integer',
+                        dotted: '.two',
+                        fixed: true,
+                        bits: 2,
+                        endianness: 'big',
+                        compliment: false,
+                        name: 'two'
+                    }, {
+                        type: 'integer',
+                        dotted: '.four',
+                        fixed: true,
+                        bits: 4,
+                        endianness: 'big',
+                        compliment: false,
+                        name: 'four'
+                    }]
+                }],
+                name: 'value',
+                dotted: '.value'
+            }]
+        }],
+        name: 'object'
+    }], 'packed variant')
 })
