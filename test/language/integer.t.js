98151afd330f8cba8404b4163a790c2185c05455
Alan Gutierrez <alan@prettyrobots.com>
2020-06-23 15:49:19 -0500

    Rename and consolidate some language tests.

---
+++ test/language/integer.t.js
@@ -0,0 +1,75 @@
+require('proof')(4, okay => {
+    const simplified = require('../../simplified')
+    okay(simplified({ packet: { value: 16 } }), [{
+        type: 'structure',
+        vivify: 'object',
+        name: 'packet',
+        fixed: true,
+        bits: 16,
+        dotted: '',
+        fields: [{
+            name: 'value',
+            dotted: '.value',
+            type: 'integer',
+            vivify: 'number',
+            fixed: true,
+            bits: 16,
+            endianness: 'big',
+            compliment: false
+        }]
+    }], 'short')
+    okay(simplified({ packet: { value: ~16 } }), [{
+        type: 'structure',
+        vivify: 'object',
+        name: 'packet',
+        fixed: true,
+        bits: 16,
+        dotted: '',
+        fields: [{
+            type: 'integer',
+            vivify: 'number',
+            name: 'value',
+            dotted: '.value',
+            fixed: true,
+            bits: 16,
+            endianness: 'little',
+            compliment: false
+        }]
+    }], 'little endian')
+    okay(simplified({ packet: { value: -16 } }), [{
+        type: 'structure',
+        vivify: 'object',
+        name: 'packet',
+        fixed: true,
+        bits: 16,
+        dotted: '',
+        fields: [{
+            type: 'integer',
+            vivify: 'number',
+            name: 'value',
+            dotted: '.value',
+            fixed: true,
+            bits: 16,
+            endianness: 'big',
+            compliment: true
+        }]
+    }], 'two\'s compliment')
+    okay(simplified({ packet: { value: -~16 } }), [{
+        type: 'structure',
+        vivify: 'object',
+        name: 'packet',
+        fixed: true,
+        bits: 16,
+        dotted: '',
+        fields: [{
+            type: 'integer',
+            vivify: 'number',
+            name: 'value',
+            dotted: '.value',
+            fixed: true,
+            bits: 16,
+            endianness: 'little',
+            compliment: true
+        }]
+    }], 'two\'s compliment little endian')
+})
