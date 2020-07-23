e16340bef03b589b0dba1065830225a2375f4d24
Alan Gutierrez <alan@prettyrobots.com>
2020-07-23 17:13:47 -0500

    Move packing logic into single function.
    Allows us to maintain variables for a single integer while packing and
    unpacking without having to pass as much around when we have to recurse.
    
    See #563.

--- pack.js
+++ pack.js
@@ -6,10 +6,6 @@ const inliner = require('./inliner')
 
 const fiddle = require('./fiddle/pack')
 
-function _fiddle (pack) {
-    return fiddle(pack.bits, pack.offset, pack.size, pack.value)
-}
-
 function flatten (flattened, path, fields, assignment = '=') {
     for (const field of fields) {
         switch (field.type) {
@@ -62,97 +58,105 @@ function flatten (flattened, path, fields, assignment = '=') {
     return flattened
 }
 
-function subPack (accumulate, root, path, bits, offset, fields) {
-    const packed = [[]]
-    for (const field of fields) {
-        switch (field.type) {
-        case 'integer': {
-                packed[0].push({
-                    bits: bits,
-                    offset: offset,
-                    size: field.bits,
-                    value: field.value
-                })
-                offset += field.bits
-            }
-            break
-        case 'conditional': {
-                const { conditional, path, assignment } = field
-                let ladder = '', keywords = 'if'
-                for (let i = 0, I = conditional.serialize.conditions.length; i < I; i++) {
-                    const condition = conditional.serialize.conditions[i]
-                    const source = module.exports.call(null, accumulate, root, {
-                        bits: bits,
-                        fields: condition.fields[0].type == 'integer' && condition.fields[0].fields
-                              ? condition.fields[0].fields
-                              : condition.fields
-                    }, path, '$_', assignment, offset)
-                    ladder = condition.test != null ? function () {
-                        const inline = inliner(accumulate, path, [ condition.test ], [])
-                        return $(`
-                            `, ladder, `${keywords} (`, inline.inlined.shift(), `) {
-                                `, source, `
-                            }
-                        `)
-                    } () : $(`
-                        `, ladder, ` else {
-                            `, source, `
-                        }
-                    `)
-                    keywords = ' else if'
-                }
-                offset += conditional.bits
-                packed.unshift(ladder)
-            }
-            break
-        case 'switch': {
-                const { switched, path, assignment } = field, block = []
-                const cases = []
-                for (const when of switched.cases) {
-                    const source = module.exports.call(null, accumulate, root, {
-                        bits: bits,
-                        fields: when.fields[0].type == 'integer' && when.fields[0].fields
-                              ? when.fields[0].fields
-                              : when.fields
-                    }, path, '$_', assignment, offset)
-                    cases.push($(`
-                        ${when.otherwise ? 'default' : `case ${JSON.stringify(when.value)}`}:
-
-                            `, source, `
-
-                            break
-                    `))
-                }
-                const inlined = inliner(accumulate, path, [ switched.select ], [])
-                const select = switched.stringify
-                    ? `String(${inlined.inlined.shift()})`
-                    : inlined.inlined.shift()
-                packed.unshift($(`
-                    switch (`, select ,`) {
-                    `, join(cases), `
-                    }
-                `))
-            }
-            break
-        }
-    }
-    return packed.reverse()
-}
 
 // A recent implementation of packing, but one that is now untested and stale.
 // Removing from the `serialize.all` generator for visibility.
 module.exports = function (accumulate, root, field, path, stuff, assignment = '=', offset = 0) {
-    const block = [], flattened = flatten([], path, field.fields, assignment)
-    for (const packed of subPack(accumulate, root, path, field.bits, offset, flattened)) {
-        if (typeof packed == 'string') {
-            block.push(packed)
-        } else if (packed.length != 0) {
-            block.push($(`
-                ${stuff} ${assignment}
-                    `, packed.map(_fiddle).join(' |\n'), `
-            `))
+    function pack (accumulate, root, path, bits, offset, fields) {
+        const packed = [[]]
+        for (const field of fields) {
+            switch (field.type) {
+            case 'integer': {
+                    packed[0].push({
+                        bits: bits,
+                        offset: offset,
+                        size: field.bits,
+                        value: field.value
+                    })
+                    offset += field.bits
+                }
+                break
+            case 'conditional': {
+                    const { conditional, path, assignment } = field
+                    let ladder = '', keywords = 'if'
+                    for (let i = 0, I = conditional.serialize.conditions.length; i < I; i++) {
+                        const condition = conditional.serialize.conditions[i]
+                        const source = generate(accumulate, root, {
+                            bits: bits,
+                            fields: condition.fields[0].type == 'integer' && condition.fields[0].fields
+                                  ? condition.fields[0].fields
+                                  : condition.fields
+                        }, path, '$_', assignment, offset)
+                        ladder = condition.test != null ? function () {
+                            const inline = inliner(accumulate, path, [ condition.test ], [])
+                            return $(`
+                                `, ladder, `${keywords} (`, inline.inlined.shift(), `) {
+                                    `, source, `
+                                }
+                            `)
+                        } () : $(`
+                            `, ladder, ` else {
+                                `, source, `
+                            }
+                        `)
+                        keywords = ' else if'
+                    }
+                    offset += conditional.bits
+                    packed.unshift(ladder)
+                }
+                break
+            case 'switch': {
+                    const { switched, path, assignment } = field, block = []
+                    const cases = []
+                    for (const when of switched.cases) {
+                        const source = generate(accumulate, root, {
+                            bits: bits,
+                            fields: when.fields[0].type == 'integer' && when.fields[0].fields
+                                  ? when.fields[0].fields
+                                  : when.fields
+                        }, path, '$_', assignment, offset)
+                        cases.push($(`
+                            ${when.otherwise ? 'default' : `case ${JSON.stringify(when.value)}`}:
+
+                                `, source, `
+
+                                break
+                        `))
+                    }
+                    const inlined = inliner(accumulate, path, [ switched.select ], [])
+                    const select = switched.stringify
+                        ? `String(${inlined.inlined.shift()})`
+                        : inlined.inlined.shift()
+                    packed.unshift($(`
+                        switch (`, select ,`) {
+                        `, join(cases), `
+                        }
+                    `))
+                }
+                break
+            }
         }
-        assignment = '|='
+        return packed.reverse()
     }
-    return join(block)
+
+    function generate (accumulate, root, field, path, stuff, assignment = '=', offset = 0) {
+        const block = [], flattened = flatten([], path, field.fields, assignment)
+        for (const packed of pack(accumulate, root, path, field.bits, offset, flattened)) {
+            if (typeof packed == 'string') {
+                block.push(packed)
+            } else if (packed.length != 0) {
+                const fiddles = packed.map(pack => {
+                    return fiddle(pack.bits, pack.offset, pack.size, pack.value)
+                })
+                block.push($(`
+                    ${stuff} ${assignment}
+                        `, fiddles.join(' |\n'), `
+                `))
+            }
+            assignment = '|='
+        }
+        return join(block)
+    }
+
+    return generate(accumulate, root, field, path, stuff, assignment, offset)
 }
--- unpack.js
+++ unpack.js
@@ -7,143 +7,143 @@ const { structure } = require('./vivify')
 // Generate inline function source.
 const inliner = require('./inliner')
 
-function _fiddle (pack) {
-    return fiddle(pack.bits, pack.offset, pack.size, pack.packed)
-}
-
 function unpack (accumulate, root, path, field, packed, offset = 0) {
-    let bits = field.bits, bit = offset
-    function advance (size) {
-        const offset = bit
-        bit += size
-        return offset
-    }
-    function unpack (path, field) {
-        switch (field.type) {
-        case 'checkpoint':
-            return field
-        case 'literal':
-            bit += field.before.bits || 0
-            const unpacked = unpack(path + field.dotted, field.fields[0])
-            bit += field.after.bits || 0
-            return unpacked
-        case 'integer':
-            return {
-                type: 'integer',
-                bits: bits,
-                path: path + field.dotted,
-                packed: packed,
-                size: field.bits,
-                offset: advance(field.bits),
-                compliment: field.compliment
-            }
-        case 'conditional':
-            return {
-                type: 'conditional',
-                bits: bits,
-                path: path + field.dotted,
-                packed: packed,
-                offset: bit,
-                size: advance(field.bits),
-                conditional: field
-            }
-        case 'switch':
-            return {
-                type: 'switch',
-                bits: bits,
-                path: path + field.dotted,
-                packed: packed,
-                offset: bit,
-                size: advance(field.bits),
-                switched: field
-            }
+    function generate (accumulate, root, path, field, packed, offset) {
+        let bits = field.bits, bit = offset
+        function advance (size) {
+            const offset = bit
+            bit += size
+            return offset
         }
-    }
-    const blocks = []
-    for (const packing of field.fields.map(f => unpack(path, f))) {
-        switch (packing.type) {
-        case 'checkpoint':
-            break
-        case 'integer': {
-                const assign = `${packing.path} = ${_fiddle(packing)}`
-                if (packing.compliment) {
-                    blocks.push($(`
-                        `, assign, `
-                        ${packing.path} =
-                            `, unsign(packing.path, packing.size), `
-                    `))
-                } else {
-                    blocks.push(assign)
+        function unpack (path, field) {
+            switch (field.type) {
+            case 'checkpoint':
+                return field
+            case 'literal':
+                bit += field.before.bits || 0
+                const unpacked = unpack(path + field.dotted, field.fields[0])
+                bit += field.after.bits || 0
+                return unpacked
+            case 'integer':
+                return {
+                    type: 'integer',
+                    bits: bits,
+                    path: path + field.dotted,
+                    packed: packed,
+                    size: field.bits,
+                    offset: advance(field.bits),
+                    compliment: field.compliment
+                }
+            case 'conditional':
+                return {
+                    type: 'conditional',
+                    bits: bits,
+                    path: path + field.dotted,
+                    packed: packed,
+                    offset: bit,
+                    size: advance(field.bits),
+                    conditional: field
+                }
+            case 'switch':
+                return {
+                    type: 'switch',
+                    bits: bits,
+                    path: path + field.dotted,
+                    packed: packed,
+                    offset: bit,
+                    size: advance(field.bits),
+                    switched: field
                 }
             }
-            break
-        case 'conditional': {
-                const { conditional, path, offset } = packing
-                let ladder = '', keywords = 'if'
-                for (let i = 0, I = conditional.serialize.conditions.length; i < I; i++) {
-                    const condition = conditional.serialize.conditions[i]
-                    // TODO Can we use the univeral vivify here? (Ugh.)
-                    const vivifyed = condition.fields[0].type == 'integer' && condition.fields[0].fields
-                    const source = module.exports.call(null, accumulate, root, path, {
-                        bits: bits,
-                        fields: vivifyed ? condition.fields[0].fields : condition.fields
-                    }, packed, offset)
-                    const vivify = vivifyed ? structure(path, condition.fields[0]) : null
-                    ladder = condition.test != null ? function () {
-                        const inline = inliner(accumulate, path, [ condition.test ], [])
-                        return $(`
-                            `, ladder, `${keywords} (`, inline.inlined.shift(), `) {
+        }
+        const blocks = []
+        for (const packing of field.fields.map(f => unpack(path, f))) {
+            switch (packing.type) {
+            case 'checkpoint':
+                break
+            case 'integer': {
+                    const fiddled = fiddle(packing.bits, packing.offset, packing.size, packing.packed)
+                    const assign = `${packing.path} = ${fiddled}`
+                    if (packing.compliment) {
+                        blocks.push($(`
+                            `, assign, `
+                            ${packing.path} =
+                                `, unsign(packing.path, packing.size), `
+                        `))
+                    } else {
+                        blocks.push(assign)
+                    }
+                }
+                break
+            case 'conditional': {
+                    const { conditional, path, offset } = packing
+                    let ladder = '', keywords = 'if'
+                    for (let i = 0, I = conditional.serialize.conditions.length; i < I; i++) {
+                        const condition = conditional.serialize.conditions[i]
+                        // TODO Can we use the univeral vivify here? (Ugh.)
+                        const vivifyed = condition.fields[0].type == 'integer' && condition.fields[0].fields
+                        const source = generate.call(null, accumulate, root, path, {
+                            bits: bits,
+                            fields: vivifyed ? condition.fields[0].fields : condition.fields
+                        }, packed, offset)
+                        const vivify = vivifyed ? structure(path, condition.fields[0]) : null
+                        ladder = condition.test != null ? function () {
+                            const inline = inliner(accumulate, path, [ condition.test ], [])
+                            return $(`
+                                `, ladder, `${keywords} (`, inline.inlined.shift(), `) {
+                                    `, vivify, -1, `
+
+                                    `, source, `
+                                }
+                            `)
+                        } () : $(`
+                            `, ladder, ` else {
                                 `, vivify, -1, `
 
                                 `, source, `
                             }
                         `)
-                    } () : $(`
-                        `, ladder, ` else {
-                            `, vivify, -1, `
-
-                            `, source, `
-                        }
-                    `)
-                    keywords = ' else if'
+                        keywords = ' else if'
+                    }
+                    blocks.push(ladder)
                 }
-                blocks.push(ladder)
-            }
-            break
-        case 'switch': {
-                const { switched, path, offset } = packing, block = []
-                const cases = []
-                for (const when of switched.cases) {
-                    const vivifyed = when.fields[0].type == 'integer' && when.fields[0].fields
-                    const source = module.exports.call(null, accumulate, root, path, {
-                        bits: bits,
-                        fields: vivifyed ? when.fields[0].fields : when.fields
-                    }, packed, offset)
-                    const vivify = vivifyed ? structure(path, when.fields[0]) : null
-                    cases.push($(`
-                        ${when.otherwise ? 'default' : `case ${JSON.stringify(when.value)}`}:
-                            `, vivify, -1, `
+                break
+            case 'switch': {
+                    const { switched, path, offset } = packing, block = []
+                    const cases = []
+                    for (const when of switched.cases) {
+                        const vivifyed = when.fields[0].type == 'integer' && when.fields[0].fields
+                        const source = module.exports.call(null, accumulate, root, path, {
+                            bits: bits,
+                            fields: vivifyed ? when.fields[0].fields : when.fields
+                        }, packed, offset)
+                        const vivify = vivifyed ? structure(path, when.fields[0]) : null
+                        cases.push($(`
+                            ${when.otherwise ? 'default' : `case ${JSON.stringify(when.value)}`}:
+                                `, vivify, -1, `
 
-                            `, source, `
+                                `, source, `
 
-                            break
+                                break
+                        `))
+                    }
+                    const inlined = inliner(accumulate, path, [ switched.select ], [])
+                    const select = switched.stringify
+                        ? `String(${inlined.inlined.shift()})`
+                        : inlined.inlined.shift()
+                    blocks.push($(`
+                        switch (`, select, `) {
+                        `, join(cases), `
+                        }
                     `))
                 }
-                const inlined = inliner(accumulate, path, [ switched.select ], [])
-                const select = switched.stringify
-                    ? `String(${inlined.inlined.shift()})`
-                    : inlined.inlined.shift()
-                blocks.push($(`
-                    switch (`, select, `) {
-                    `, join(cases), `
-                    }
-                `))
+                break
             }
-            break
+            offset += packing.bits
         }
-        offset += packing.bits
+        return join(blocks)
     }
-    return join(blocks)
+    return generate(accumulate, root, path, field, packed, offset)
 }
 
 module.exports = unpack
