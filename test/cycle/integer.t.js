d139b511232c084950e956929f9b0010d62dfb4c
Alan Gutierrez <alan@prettyrobots.com>
2020-07-28 13:03:07 -0500

    Tidy integer cycle test.

--- test/cycle/integer.t.js
+++ test/cycle/integer.t.js
@@ -1,7 +1,8 @@
 require('proof')(0, prove)
 
 function prove (okay) {
-    require('./cycle')(okay, {
+    const cycle = require('./cycle')
+    cycle(okay, {
         name: 'integer/byte/whole',
         define: {
             object: { word: 8 }
@@ -9,7 +10,7 @@ function prove (okay) {
         objects: [{ word: 0xab }]
     })
     /* TODO
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/byte/negative',
         define: {
             object: { word: -8 }
@@ -17,84 +18,84 @@ function prove (okay) {
         objects: [{ word: -0x7b }]
     })
     */
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/be/word/short',
         define: {
             object: { value: 16 }
         },
         objects: [{ value: 0xabcd }]
     })
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/be/word/int',
         define: {
             object: { value: 32 }
         },
         objects: [{ value: 0xabcdef01 }]
     })
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/be/word/long',
         define: {
             object: { value: 64 }
         },
         objects: [{ value: 0xaaaaaaaaaaaaaaaan }]
     })
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/be/compliment/short',
         define: {
             object: { value: -16 }
         },
         objects: [{ value: 0x7bcd }]
     })
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/be/compliment/int',
         define: {
             object: { value: -32 }
         },
         objects: [{ value: 0x7bcdef01 }]
     })
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/be/compliment/long',
         define: {
             object: { value: -64 }
         },
         objects: [{ value: -0x7aaaaaaaaaaaaaaan }]
     })
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/le/word/short',
         define: {
             object: { value: ~16 }
         },
         objects: [{ value: 0xabcd }]
     })
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/le/word/int',
         define: {
             object: { value: ~32 }
         },
         objects: [{ value: 0xabcdef01 }]
     })
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/le/word/long',
         define: {
             object: { value: ~64 }
         },
         objects: [{ value: 0xaaaaaaaaaaaaaaaan }]
     })
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/le/compliment/short',
         define: {
             object: { value: -~16 }
         },
         objects: [{ value: -0x7bcd }]
     })
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/le/compliment/int',
         define: {
             object: { value: -~32 }
         },
         objects: [{ value: -0x7bcdef01 }]
     })
-    require('./cycle')(okay, {
+    cycle(okay, {
         name: 'integer/le/compliment/long',
         define: {
             object: { value: -~64 }
