e3fef166330a850adb9818d7ca9f377b6e05531e
Alan Gutierrez <alan@prettyrobots.com>
2021-09-08 18:12:03 -0500

    Pull parameters into Edify.
    And now the `swipe.md` is gone.
    
    See #471.

---
+++ test/readme/parameters.js
@@ -0,0 +1,244 @@
+const sizeOf = {
+    object: function () {
+        return function (object) {
+            let $start = 0, $accumulator = {}
+
+            $start += 1 * object.string.length + 1
+
+            return $start
+        }
+    } ()
+}
+
+const serializer = {
+    all: {
+        object: function () {
+            return function (object, $buffer, $start, {
+                encoding = 'utf8'
+            } = {}) {
+                let $i = [], $$ = [], $accumulator = {}
+
+                $accumulator['encoding'] = encoding
+
+                $$[0] = (value => Buffer.from(value, encoding))(object.string, object)
+
+                $$[0].copy($buffer, $start, 0, $$[0].length)
+                $start += $$[0].length
+
+                $buffer[$start++] = 0x0
+
+                return { start: $start, serialize: null }
+            }
+        } ()
+    },
+    inc: {
+        object: function () {
+            return function (object, {
+                encoding = 'utf8'
+            } = {}, $step = 0, $i = [], $$ = [], $accumulator = {}) {
+                let $_
+
+                return function $serialize ($buffer, $start, $end) {
+                    for (;;) {
+                        switch ($step) {
+                        case 0:
+
+                            $accumulator['encoding'] = encoding
+
+                        case 1:
+
+                            $$[0] = (value => Buffer.from(value, encoding))(object.string, object)
+
+                        case 2:
+
+                            $_ = 0
+
+                        case 3: {
+
+                                const length = Math.min($end - $start, $$[0].length - $_)
+                                $$[0].copy($buffer, $start, $_, $_ + length)
+                                $start += length
+                                $_ += length
+
+                                if ($_ != $$[0].length) {
+                                    $step = 3
+                                    return { start: $start, serialize: $serialize }
+                                }
+
+                            }
+
+                        case 4:
+
+                            if ($start == $end) {
+                                $step = 4
+                                return { start: $start, serialize: $serialize }
+                            }
+
+                            $buffer[$start++] = 0x0
+
+                        }
+
+                        break
+                    }
+
+                    return { start: $start, serialize: null }
+                }
+            }
+        } ()
+    }
+}
+
+const parser = {
+    all: {
+        object: function () {
+            return function ($buffer, $start, {
+                encoding = 'utf8'
+            } = {}) {
+                let $_, $i = [], $accumulator = {}
+
+                let object = {
+                    string: null
+                }
+
+                $accumulator['encoding'] = encoding
+
+                $_ = $buffer.indexOf(Buffer.from([ 0 ]), $start)
+                $_ = ~$_ ? $_ : $start
+                object.string = $buffer.slice($start, $_)
+                $start = $_ + 1
+
+                object.string = (value => value.toString(encoding))(object.string)
+
+                return object
+            }
+        } ()
+    },
+    inc: {
+        object: function () {
+            return function (object, {
+                encoding = 'utf8'
+            } = {}, $step = 0, $i = [], $accumulator = []) {
+                let $buffers = []
+
+                return function $parse ($buffer, $start, $end) {
+                    for (;;) {
+                        switch ($step) {
+                        case 0:
+
+                            object = {
+                                string: null
+                            }
+
+                        case 1:
+
+                            $accumulator['encoding'] = encoding
+
+                        case 2: {
+
+                            const $index = $buffer.indexOf(0x0, $start)
+                            if (~$index) {
+                                $buffers.push($buffer.slice($start, $index))
+                                $start = $index + 1
+                                $step = 3
+                                continue
+                            } else {
+                                $step = 2
+                                $buffers.push($buffer.slice($start))
+                                return { start: $end, object: null, parse: $parse }
+                            }
+
+                        }
+
+                        case 3:
+
+
+                            object.string = $buffers.length == 1 ? $buffers[0] : Buffer.concat($buffers)
+                            $buffers.length = 0
+
+                            object.string = (value => value.toString(encoding))(object.string)
+
+                        }
+
+                        return { start: $start, object: object, parse: null }
+                        break
+                    }
+                }
+            }
+        } ()
+    }
+}
+
+module.exports = {
+    sizeOf: sizeOf,
+    serializer: {
+        all: serializer.all,
+        inc: serializer.inc,
+        bff: function ($incremental) {
+            return {
+                object: function () {
+                    return function (object, {
+                        encoding = 'utf8'
+                    } = {}) {
+                        return function ($buffer, $start, $end) {
+                            let $i = [], $$ = [], $accumulator = {}
+
+                            $accumulator['encoding'] = encoding
+
+                            $$[0] = (value => Buffer.from(value, encoding))(object.string, object)
+
+                            if ($end - $start < 1 + object.string.length * 1) {
+                                return $incremental.object(object, {
+                                    encoding: encoding
+                                }, 2, $i, $$, $accumulator)($buffer, $start, $end)
+                            }
+
+                            $$[0].copy($buffer, $start, 0, $$[0].length)
+                            $start += $$[0].length
+
+                            $buffer[$start++] = 0x0
+
+                            return { start: $start, serialize: null }
+                        }
+                    }
+                } ()
+            }
+        } (serializer.inc)
+    },
+    parser: {
+        all: parser.all,
+        inc: parser.inc,
+        bff: function ($incremental) {
+            return {
+                object: function () {
+                    return function ({
+                        encoding = 'utf8'
+                    } = {}) {
+                        return function ($buffer, $start, $end) {
+                            let $_, $i = [], $accumulator = {}
+
+                            let object = {
+                                string: null
+                            }
+
+                            $accumulator['encoding'] = encoding
+
+                                                $_ = $buffer.indexOf(Buffer.from([ 0 ]), $start)
+                                                if (~$_) {
+                                                    object.string = $buffer.slice($start, $_)
+                                                    $start = $_ + 1
+                                                } else {
+                                                    return $incremental.object(object, {
+                                encoding: encoding
+                            }, 2, $i, $accumulator)($buffer, $start, $end)
+                                                }
+
+                            object.string = (value => value.toString(encoding))(object.string)
+
+                            return { start: $start, object: object, parse: null }
+                        }
+                    } ()
+                }
+            }
+        } (parser.inc)
+    }
+}
--- test/readme/readme.t.js
+++ test/readme/readme.t.js
@@ -1774,6 +1774,96 @@ require('proof')(115, async okay => {
             0x47, 0xc0, 0x40, 0x28
         ], { require: { assert: 'assert', crypto: 'crypto' } })
     }
+
+    // Here we also introduce the concept of buffer inlines. These are inlines that
+    // operate not on the serialized or parsed value, but instead on the underlying
+    // buffer. In the above example the `hash.update()` inline is not called once for
+    // each property in the `body`, it is called for each buffer chunk that contians the
+    // binary data for the `body`.
+    //
+    // Unlike odinary inline functions, a buffer inline is not called prior to
+    // serialization. Buffer inlines are called as late as possible to process as much
+    // of the buffer continguously as possible. In the previous example, the
+    // `hash.update()` inline is applied to the binary data that defines the entire
+    // `body` which it encapsulates.
+    //
+    // We use nested structures to group.
+    //
+    // **TODO**: Simplier calculation example to start. Calculation is important
+    // because it will allow us to talk about the difference between `sizeof`,
+    // `offsetof`.
+    //
+    // **TODO**: Come back and implement this by finding a way to extract sizeof and
+    // offset of. Punting because I don't really know what inspired this example or
+    // what it is supposed to illustrate.
+
+    {
+        const definition = {
+            packet: [{ counter: () => [] }, {
+                header: {
+                    type: 8,
+                    length: [[
+                        ({ $, counter }) => {
+                            return counter[0] = $sizeof.packet($) - $offsetof.packet($, 'body')
+                        }
+                    ], 16, [
+                        ({ $_, counter }) => {
+                            return counter[0] = $_
+                        }
+                    ]]
+                },
+                body: [[[
+                    ({ $start, $end, counter }) => counter[0] -= $end - $start
+                ]], {
+                    value: 32,
+                    string: [[ 8 ], 0x0 ],
+                    variable: [
+                        ({ counter }) => counter[0] == 4, 32,
+                        ({ counter }) => counter[0] == 2, 16,
+                        8
+                    ]
+                }],
+            }]
+        }
+    }
+
+    // ### Parameters
+    //
+    // **TODO**: Need first draft, or reread this and see if it is a real first draft.
+    //
+    // Accumulators described in the preceding section also define parameters. Any
+    // accumulator declared on the top most field will create parameters to the
+    // generated serializes and parsers.
+
+    {
+        const definition = {
+            object: [{ counter: [ 0 ] }, [[[
+                ({ $start, $end, counter }) => counter[0] += $end - $start
+            ]], {
+                number: 8,
+                string: [ [ 8 ], 0x0 ]
+            }]]
+        }
+        // **TODO**: API call to get counter.
+    }
+
+    // The parameters are available as both arguments that can be passed to inline
+    // functions as well as generally available in the program scope. Be careful not to
+    // careful not to hide any module declarations you've declared.
+
+    {
+        const definition = {
+            object: [{ encoding: 'utf8' }, {
+                string: [[
+                    value => Buffer.from(value, encoding)
+                ], [ [ Buffer ], 0x0 ], [
+                    value => value.toString(encoding)
+                ]]
+            }]
+        }
+        const moduleName = compile('parameters', definition, {})
+        // *TODO*: API call to encode string ascii or something.
+    }
 })
 
 // You can run this unit test yourself to see the output from the various
